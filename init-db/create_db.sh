8e5bc5c2e72497181561aa57ff132f54c057c28a
buihuy13 <buiquochuytd@gmail.com>
2026-03-19 19:13:01 +0000

    config hive and trino, init-db for services, added qdrant for vector database, deleted unused dependency in poetry

---
+++ init-db/create_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# File: init-db/01_create_databases.sh
+# PostgreSQL sẽ tự chạy file này khi container khởi động lần đầu
+ 
+set -e
+ 
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE $MLFLOW_DB;
+    CREATE DATABASE $METABASE_DB;
+    CREATE DATABASE $HIVE_DB;
+ 
+    GRANT ALL PRIVILEGES ON DATABASE $MLFLOW_DB TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE $METABASE_DB TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE $HIVE_DB TO $POSTGRES_USER;
+EOSQL
